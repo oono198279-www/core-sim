3d84b13f1865d6d4f09b5fbd0e05a1b64fbe741f
oono198279-www <oono198279@gmail.com>
2025-09-13 15:45:07 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='core-sim-v3-3';
+const CACHE='core-sim-v4';
 const ASSETS = ['./','./index.html','./app.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))); });
